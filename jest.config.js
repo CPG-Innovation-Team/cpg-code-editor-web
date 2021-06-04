8005f2508d44e572e3a78023281def7aacb9eb34
casualbill <hotwindzb@gmail.com>
2021-06-04 16:44:30 +0800

    Modify Jest config to adapt monaco-editor

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  transformIgnorePatterns: ['/node_modules/(?!monaco-editor)'],
+  moduleNameMapper: {
+    'monaco-editor': '<rootDir>/node_modules/monaco-editor/esm/vs/editor/editor.api.js',
+  },
 };
