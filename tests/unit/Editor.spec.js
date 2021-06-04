44380fa5586bdee3576296a351634bdfcb59913d
casualbill <hotwindzb@gmail.com>
2021-06-04 16:49:54 +0800

    Unit test for editor page render

---
+++ tests/unit/Editor.spec.js
@@ -0,0 +1,27 @@
+import 'jest-canvas-mock';
+import { shallowMount } from '@vue/test-utils';
+import Editor from '@/views/Editor.vue';
+
+describe('Editor.vue', () => {
+  beforeAll(() => {
+    // Mocking matchMedia method not implemented in JSDOM
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+
+  it('Render page with editor and console', () => {
+    const wrapper = shallowMount(Editor);
+
+    expect(wrapper.find('.monaco-editor').isVisible()).toBeTruthy();
+    expect(wrapper.find('.console').isVisible()).toBeTruthy();
+  });
+});
