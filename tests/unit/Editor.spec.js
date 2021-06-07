0cd81842e3ad0bb78d009162ef8b3e2d9429bb7d
casualbill <hotwindzb@gmail.com>
2021-06-07 16:52:36 +0800

    Add unit test for get/set/run code in editor

--- tests/unit/Editor.spec.js
+++ tests/unit/Editor.spec.js
@@ -18,16 +18,38 @@ describe('Editor.vue', () => {
     });
   });
 
-  it('Render page with editor and console', () => {
-    const wrapper = shallowMount(Editor, {
-      mocks: {
-        $route: {
-          params: { roomId: undefined },
-        },
+  const getWrapper = (roomId = undefined) => shallowMount(Editor, {
+    mocks: {
+      $route: {
+        params: { roomId },
       },
-    });
+    },
+  });
+
+  it('Render page with editor and console', () => {
+    const wrapper = getWrapper();
     expect.assertions(2);
     expect(wrapper.find('.monaco-editor').isVisible()).toBeTruthy();
     expect(wrapper.find('.console').isVisible()).toBeTruthy();
+  });
+
+  it('Set code and get it in editor', () => {
+    const wrapper = getWrapper();
+    wrapper.vm.setCode('TEST_CODE');
+    expect(wrapper.vm.getCode()).toBe('TEST_CODE');
+  });
+
+  it('Run javascript code console.log then add log item in log list', () => {
+    const wrapper = getWrapper();
+    wrapper.vm.setCode('console.log("TEST_LOG")');
+    wrapper.vm.runCode();
+    expect(wrapper.vm.$data.logList[0]).toEqual({ msg: 'TEST_LOG', style: undefined });
+  });
+
+  it('Run javascript error code then add error log item in log list', () => {
+    const wrapper = getWrapper();
+    wrapper.vm.setCode('TEST_ERROR');
+    wrapper.vm.runCode();
+    expect(wrapper.vm.$data.logList[0]).toEqual({ msg: 'ReferenceError: TEST_ERROR is not defined', style: 'error' });
   });
 });
