faddb85c2efe840e1a3502a0a35348e1de50d6e8
casualbill <hotwindzb@gmail.com>
2021-06-07 11:49:26 +0800

    Show coverage for unit testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
   transformIgnorePatterns: ['/node_modules/(?!monaco-editor)'],
   moduleNameMapper: {
     'monaco-editor': '<rootDir>/node_modules/monaco-editor/esm/vs/editor/editor.api.js',
