c2054989efb491d39aa8f98d33c3bb1273b1a255
casualbill <hotwindzb@gmail.com>
2021-06-16 02:00:09 +0800

    Update download code filename with date and time

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -141,8 +141,15 @@ export default {
       document.body.appendChild(downloadElement);
 
       const URL = window.URL || window.webkitURL;
+      const date = new Date();
+      const year = date.getFullYear().toString().slice(2);
+      const month = date.getMonth() < 9 ? `0${(date.getMonth() + 1).toString()}` : (date.getMonth() + 1).toString();
+      const day = date.getDate() < 10 ? `0${date.getDate().toString()}` : date.getDate().toString();
+      const hour = date.getHours() < 10 ? `0${date.getHours().toString()}` : date.getHours().toString();
+      const minute = date.getMinutes() < 10 ? `0${date.getMinutes().toString()}` : date.getMinutes().toString();
+      const fileName = `code-${year}${month}${day}-${hour}${minute}`;
       downloadElement.href = URL.createObjectURL(blob);
-      downloadElement.download = 'code';
+      downloadElement.download = fileName;
       downloadElement.click();
       document.body.removeChild(downloadElement);
     },
