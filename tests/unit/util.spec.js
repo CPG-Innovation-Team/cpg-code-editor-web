144f5ad279c1b7582b39e3b826f5fa515a7bb71c
casualbill <hotwindzb@gmail.com>
2021-06-16 17:06:33 +0800

    Add unit test for date time format in util

---
+++ tests/unit/util.spec.js
@@ -0,0 +1,11 @@
+import formattedDateTime from '../../src/util';
+
+describe('util.js', () => {
+  it('Formatt date time with single-digit month, day, hour, minute and second', () => {
+    expect(formattedDateTime(new Date('2021-09-09 01:02:03'))).toBe('210909-010203');
+  });
+
+  it('Formatt date time with double-digit month, day, hour, minute and second', () => {
+    expect(formattedDateTime(new Date('2021-10-20 11:22:33'))).toBe('211020-112233');
+  });
+});
