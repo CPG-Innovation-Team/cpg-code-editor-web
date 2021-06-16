e46d6365e637533ad39f286aa96b82a409d11db3
casualbill <hotwindzb@gmail.com>
2021-06-16 16:44:31 +0800

    Add second for time and correct function name

--- src/util.js
+++ src/util.js
@@ -1,10 +1,11 @@
-const formattedDate = (date) => {
+const formattedDateTime = (date) => {
   const year = date.getFullYear().toString().slice(2);
   const month = date.getMonth() < 9 ? `0${(date.getMonth() + 1).toString()}` : (date.getMonth() + 1).toString();
   const day = date.getDate() < 10 ? `0${date.getDate().toString()}` : date.getDate().toString();
   const hour = date.getHours() < 10 ? `0${date.getHours().toString()}` : date.getHours().toString();
   const minute = date.getMinutes() < 10 ? `0${date.getMinutes().toString()}` : date.getMinutes().toString();
-  return `${year}${month}${day}-${hour}${minute}`;
+  const second = date.getSeconds() < 10 ? `0${date.getSeconds().toString()}` : date.getSeconds().toString();
+  return `${year}${month}${day}-${hour}${minute}${second}`;
 };
 
-export default formattedDate;
+export default formattedDateTime;
