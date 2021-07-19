384d05a1ef685d75f7bfae768acce6cf84776c19
Yuanbo Jiang <blindmalus@wxsec.cn>
2021-07-19 22:13:35 +0800

    made work with nav-bar directing to editor

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,9 @@
   <v-app-bar app color="primary">
     <v-toolbar-title class="white--text">正大集团</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn class="white--text" color="blueBtn"> <v-icon>mdi-plus-box</v-icon>Create</v-btn>
+    <router-link :to="{ path: '/:roomId' }" class="nav-link"
+      ><v-btn class="white--text" color="blueBtn"><v-icon>mdi-plus-box</v-icon>Create</v-btn></router-link
+    >
   </v-app-bar>
 </template>
 
@@ -12,3 +14,9 @@ export default {
   data: () => ({}),
 };
 </script>
+
+<style lang="scss" scoped>
+.nav-link {
+  text-decoration: none;
+}
+</style>
