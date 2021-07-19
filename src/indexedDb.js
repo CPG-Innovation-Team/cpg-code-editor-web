b515be5b81145dae90e006db657c8c16e8cbf172
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-07-18 21:07:39 -0700

    install vuetify

--- src/indexedDb.js
+++ src/indexedDb.js
@@ -1,37 +1,37 @@
-const dbHandler = () => new Promise((resolve) => {
-  const dbRequest = window.indexedDB.open('cpgCodeEditor');
+const dbHandler = () =>
+  new Promise((resolve) => {
+    const dbRequest = window.indexedDB.open('cpgCodeEditor');
 
-  dbRequest.onsuccess = () => {
-    resolve(dbRequest.result);
-  };
+    dbRequest.onsuccess = () => {
+      resolve(dbRequest.result);
+    };
 
-  dbRequest.onupgradeneeded = (event) => {
-    const db = event.target.result;
-    if (!db.objectStoreNames.contains('codeRecord')) {
-      db.createObjectStore('codeRecord', { keyPath: 'roomId' });
-    }
-  };
-});
-
-export const updateCodeInLocalDb = (code, roomId) => {
-  dbHandler().then((db) => {
-    db.transaction(['codeRecord'], 'readwrite')
-      .objectStore('codeRecord')
-      .put({ roomId, code, updateTime: Date.now() });
-  });
-};
-
-export const getCodeInLocalDb = (roomId) => new Promise((resolve) => {
-  dbHandler().then((db) => {
-    const transaction = db.transaction(['codeRecord']);
-    const objectStore = transaction.objectStore('codeRecord');
-    const request = objectStore.get(roomId);
-
-    request.onsuccess = () => {
-      if (request.result) {
-        console.log(request.result.code);
-        resolve(request.result.code);
+    dbRequest.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains('codeRecord')) {
+        db.createObjectStore('codeRecord', { keyPath: 'roomId' });
       }
     };
   });
-});
+
+export const updateCodeInLocalDb = (code, roomId) => {
+  dbHandler().then((db) => {
+    db.transaction(['codeRecord'], 'readwrite').objectStore('codeRecord').put({ roomId, code, updateTime: Date.now() });
+  });
+};
+
+export const getCodeInLocalDb = (roomId) =>
+  new Promise((resolve) => {
+    dbHandler().then((db) => {
+      const transaction = db.transaction(['codeRecord']);
+      const objectStore = transaction.objectStore('codeRecord');
+      const request = objectStore.get(roomId);
+
+      request.onsuccess = () => {
+        if (request.result) {
+          console.log(request.result.code);
+          resolve(request.result.code);
+        }
+      };
+    });
+  });
