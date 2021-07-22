bce9e99731c005fa1ed80a9a4aa28c78a0ce87b2
fork1028 <xinyue_chen@icloud.com>
2021-07-22 11:31:09 +0800

    make the user avatar in toolbar unclickable

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -79,21 +79,14 @@
           <Profile />
         </v-menu>
 
-        <v-list-item>
+        <v-list-item disabled>
           <v-list-item-content>
             <v-avatar v-if="getUserAvatar || userAvatar">
-              <v-tooltip nudge-left="10" left>
-                <template v-slot:activator="{ on, attrs }">
-                  <img
-                    class="user-avatar ml-1 mr-1"
-                    outlined
-                    :src="require('../assets/img-' + (getUserAvatar || userAvatar) + '.png')"
-                    v-bind="attrs"
-                    v-on="on"
-                  />
-                </template>
-                <span>{{ getUserName || userName }}</span>
-              </v-tooltip>
+              <img
+                class="user-avatar ml-1 mr-1"
+                outlined
+                :src="require('../assets/img-' + (getUserAvatar || userAvatar) + '.png')"
+              />
             </v-avatar>
             <v-icon v-else>mdi-account-circle</v-icon>
           </v-list-item-content>
