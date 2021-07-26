8592d34ea7c0fae49e4f3274690071f6e490466f
fork1028 <xinyue_chen@icloud.com>
2021-07-26 13:31:59 +0800

    add apollo client in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -6,8 +8,17 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    uri: 'http://localhost:3000/api',
+  }),
+});
+
+Vue.use(VueApollo);
+
 new Vue({
   router,
   vuetify,
+  apolloProvider,
   render: (h) => h(App),
 }).$mount('#app');
