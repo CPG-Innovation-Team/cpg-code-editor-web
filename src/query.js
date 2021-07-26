528695e9c5933652e58c69ac10552c80f0b1d763
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-07-26 14:36:06 -0700

    edited query file

--- src/query.js
+++ src/query.js
@@ -1,5 +1,38 @@
 import gql from 'graphql-tag';
 
+export const SEARCH_PROJECT = gql`
+  query ($_id: String, $hash: String) {
+    project(_id: $_id, hash: $hash) {
+      _id
+      hash
+      projectName
+      code
+      createTime
+      updateTime
+      syntax
+      createUser
+      lastModifiedUser
+    }
+  }
+`;
+
+export const CREATE_PROJECT = gql`
+  mutation ($projectName: String!, $syntax: String!, $userId: String!) {
+    createUser(projectName: $projectName, syntax: $syntax, userId: $userId) {
+      success
+      data
+    }
+  }
+`;
+
+export const REMOVE_PROJECT = gql`
+  mutation ($id: String!) {
+    createUser(id: $id) {
+      result
+    }
+  }
+`;
+
 export const CREATE_USER = gql`
   mutation ($userName: String!, $avatar: String!) {
     createUser(userName: $userName, avatar: $avatar) {
