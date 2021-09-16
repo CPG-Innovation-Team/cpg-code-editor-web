13c0f0e4128c152ec5c5431e77fd328f7886d712
Yuanbo Jiang <blindmalus@wxsec.cn>
2021-09-16 13:54:53 +0800

    change title

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'CP Group Code Editor',
+    },
+  },
   lintOnSave: false,
 
   configureWebpack: {
